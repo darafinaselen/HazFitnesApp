1b1fdd2a387c52a79becec445728394ce074a26a
Rizaldi Abyannata <aldizar255@gmail.com>
2025-12-25 02:44:03 +0800

    feat: Add MainApplication class for Expo integration

---
+++ android/app/src/main/java/com/hazlabfitnessapp/MainApplication.kt
@@ -0,0 +1,36 @@
+package com.hazlabfitnessapp
+
+import android.app.Application
+import android.content.res.Configuration
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactHost: ReactHost by lazy {
+    ReactNativeHostWrapper(this, ReactNativeHostWrapper.createReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        },
+    ))
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    loadReactNative(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
+}
